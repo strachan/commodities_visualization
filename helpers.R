51e62688584637a2dda5162d4906a80589888808
Guilherme Strachan <guilhermestrachan@gmail.com>
2018-04-26 16:50:07 -0400

    create helpers file to interact with database

---
+++ helpers.R
@@ -0,0 +1,19 @@
+
+dbConnector <- function(dbname) {
+  require(RSQLite)
+  # set up connection to a database
+  conn <- dbConnect(drv = SQLite(),
+                    dbname = dbname)
+  # return conn
+  conn
+}
+
+dbGetData <- function(conn, commodity_id) {
+  # query to select all data filtering by commodity
+  query <- paste('SELECT trade.*, commodity.name FROM trade',
+                 'JOIN commodity ON trade.commodity_id = commodity.id',
+                 'WHERE commodity.id =',
+                 commodity_id)
+  as.data.table(dbGetQuery(conn = conn,
+                           statement = query))
+}
