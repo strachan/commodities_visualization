28b5a15bb0ca2768eefdcfea6b567205d5b83dd1
Guilherme Strachan <guilhermestrachan@gmail.com>
2018-04-26 23:36:14 -0400

    create queries to get the domain tables
    We modify the helpers file to create two more queries. The first gets all the commodities in the domain table and the second gets all the categories.

--- helpers.R
+++ helpers.R
@@ -10,10 +10,24 @@ dbConnector <- function(dbname) {
 
 dbGetData <- function(conn, commodity_id) {
   # query to select all data filtering by commodity
-  query <- paste('SELECT trade.*, commodity.name FROM trade',
+  query <- paste('SELECT trade.*, commodity.commodity FROM trade',
                  'JOIN commodity ON trade.commodity_id = commodity.id',
                  'WHERE commodity.id =',
                  commodity_id)
+  as.data.table(dbGetQuery(conn = conn,
+                           statement = query))
+}
+
+dbGetCommodities <- function(conn) {
+  # query to get all commodities from domain table
+  query <- 'SELECT * FROM commodity'
+  as.data.table(dbGetQuery(conn = conn,
+                           statement = query))
+}
+
+dbGetCategories <- function(conn) {
+  # query to get all categories from domain table
+  query <- 'SELECT * FROM category'
   as.data.table(dbGetQuery(conn = conn,
                            statement = query))
 }
