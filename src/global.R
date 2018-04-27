70cbe38218bbfa192a858f99cbe9682d1b429d8d
Guilherme Strachan <guilhermestrachan@gmail.com>
2018-04-27 14:28:38 -0400

    order commodity selection

--- src/global.R
+++ src/global.R
@@ -2,5 +2,5 @@
 source('./helpers.R')
 
 domainTables <- dbGetAllDomainTables(dbname = './commodities.sqlite')
-commodities <- domainTables[[1]]
+commodities <- domainTables[[1]][order(-commodity)] 
 categories <- domainTables[[2]]
