75dcebdd84d18c6dccd441f33d0f0ae02c2bb3ea
Guilherme Strachan <guilhermestrachan@gmail.com>
2018-04-26 23:33:58 -0400

    modify pre processing file to reflect changes in database
    Since we restructured the database we must reflect those changes in the pre processing file. The preprocess file that populates the database.

--- preProc.R
+++ preProc.R
@@ -13,23 +13,10 @@ tblname3 = "category"
 data <- fread(input = csvpath,
               sep = ",",
               header = TRUE)
-# select the unique values of commodity to create an id for each one
-commodity = unique(data$commodity)
-commodity_df = as.data.table(data.frame(id = 1:length(commodity), commodity = commodity))
 
 # select the unique values of commodity to create an id for each one
 category = unique(data$category)
-category_df = as.data.table(data.frame(id = 1:length(category), category = category))
-category_df[1:(nrow(category_df) - 1), 'category'] = sapply(category_df[1:(nrow(category_df) - 1), 'category'], substring, first = 4)
-
-# include the commodity's ids in the original data table
-# set the ON clause as keys of the tables:
-setkey(data,commodity)
-setkey(commodity_df,commodity)
-# perform the join using the merge function
-data = merge(data,commodity_df, all.x=TRUE)
-# rename the column to be commodity_id instead of id
-data = data %>% rename(commodity_id = id)
+category_df = data.table(id = 1:length(category), category = category)
 
 # include the category's ids in the original data table
 # set the ON clause as keys of the tables:
@@ -40,7 +27,24 @@ data = merge(data,category_df, all.x=TRUE)
 # rename the column to be category_id instead of id
 data = data %>% rename(category_id = id)
 
-data = data %>% select(country_or_area, year, flow, trade_usd, commodity_id, category_id)
+# select the unique values of commodity to create an id for each one
+commodity = unique(data %>% select(commodity, category_id))
+commodity_df = data.table(id = 1:nrow(commodity), commodity = commodity$commodity, category_id = commodity$category_id)
+
+# include the commodity's ids in the original data table
+# set the ON clause as keys of the tables:
+setkey(data,commodity)
+setkey(commodity_df,commodity)
+# perform the join using the merge function
+data = merge(data,commodity_df, all.x=TRUE)
+# rename the column to be commodity_id instead of id
+data = data %>% rename(commodity_id = id)
+
+# remove the number from the categories
+category_df[1:(nrow(category_df) - 1), 'category'] = sapply(category_df[1:(nrow(category_df) - 1), 'category'], substring, first = 4)
+
+# select columns that will be used to store in the database
+data = data %>% select(country_or_area, year, flow, trade_usd, commodity_id)
 
 ## connect to database
 conn <- dbConnect(drv = SQLite(), 
