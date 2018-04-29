25f4ee9bf344bcc1888890e4214a097b9bb9b437
Guilherme Strachan <guilhermestrachan@gmail.com>
2018-04-29 18:03:35 -0400

    create more global domain variables

--- src/global.R
+++ src/global.R
@@ -2,5 +2,8 @@
 source('./helpers.R')
 
 domainTables <- dbGetAllDomainTables(dbname = './commodities.sqlite')
-commodities <- domainTables[[1]][order(-commodity)] 
-categories <- domainTables[[2]]
+commodities <- domainTables[[1]][order(commodity)] 
+categories <- domainTables[[2]][order(category)]
+flow_types <- c('Export', 'Import', 'Balance')
+countries <- domainTables[[3]]
+years <- domainTables[[4]]
--- src/helpers.R
+++ src/helpers.R
@@ -15,6 +15,22 @@ dbGetCategories <- function(conn) {
                            statement = query))
 }
 
+dbGetCountries <- function(conn) {
+  require(data.table)
+  # query to get all countries from domain table
+  query <- 'SELECT DISTINCT trade.country_or_area FROM trade'
+  as.data.table(dbGetQuery(conn = conn,
+                           statement = query))
+}
+
+dbGetYears <- function(conn) {
+  require(data.table)
+  # query to get all years from domain table
+  query <- 'SELECT DISTINCT trade.year FROM trade'
+  as.data.table(dbGetQuery(conn = conn,
+                           statement = query))
+}
+
 dbGetAllDomainTables <- function(dbname) {
   require(RSQLite)
   # set up connection to a database
@@ -23,10 +39,12 @@ dbGetAllDomainTables <- function(dbname) {
   # get the domain tables
   commodity <- dbGetCommodities(conn)
   category <- dbGetCategories(conn)
+  countries <- dbGetCountries(conn)
+  years <- dbGetYears(conn)
   # close the connection
   dbDisconnect(conn)
   # return the values
-  list(commodity, category)
+  list(commodity, category, countries, years)
 }
 
 dbConnector <- function(session, dbname) {
